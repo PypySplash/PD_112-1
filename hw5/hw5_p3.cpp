2651b2871c33dea71bd826426e73d97cd098f200
PeterPan <alo570024@gmail.com>
2023-10-06 02:11:58 +0800

    hw5_p3 revised

--- hw5/hw5_p3.cpp
+++ hw5/hw5_p3.cpp
@@ -2,71 +2,73 @@
 #include <cstdlib>
 #include <cstring>
 using namespace std;
+const int MAX_SENTENCE_LEN = 1001;  // 長度不超過 1000，再加上 "\0"
+const int MAX_OUTPUT_LEN = 2001;  // 最壞的情況每個地方都要調整
 
 void stringProcessing(char* input, char* output);
 
 int main()
 {
-    char input[2000];
-    cin.getline(input, 2000);
-    char output[2000];
+    char input[MAX_SENTENCE_LEN];
+    cin.getline(input, MAX_SENTENCE_LEN);
+    char output[MAX_OUTPUT_LEN];
     
     stringProcessing(input, output);
-
     cout << output << "\n";
-
-    return 0;
 }
 
 void stringProcessing(char* input, char* output) 
 {
     int index = 0;
     char* p = input;
-    int count = 0;
+    // 只要 pointer *p 還在句子範圍內
     while (*p != '\0')
     {
+        // 處理找到左括號的情況
         if (*p == '(') 
         {
-            // 若左括號 ( 前方沒有空格，則空一格
+            // 若左括號前方沒有空格，後方有空格
             if (*(p - 1) != ' ' && *(p + 1) == ' ') 
             {
                 output[index] = ' ';
                 output[index + 1] = *p;
-                index += 2;
-                p++;
-            }
-            else if (*(p - 1) == ' ' && *(p + 1) == ' ')
-            {
-                output[index] = *p;
                 index++;
                 p++;
             }
+            // 若左括號前方有空格，後方也有空格
+            else if (*(p - 1) == ' ' && *(p + 1) == ' ')
+            {
+                output[index] = *p;
+                p++;
+            }
+            // 若左括號前方沒有空格，後方也沒有空格
             else if (*(p - 1) != ' ' && *(p + 1) != ' ')
             {
                 output[index] = ' ';
                 output[index + 1] = *p;
-                index += 2;
+                index ++;
             }
-            else if (*(p - 1) == ' ' && *(p + 1) != ' ')
-            {
+            // 若左括號前方有空格，後方沒有空格
+            else if (*(p - 1) == ' ' && *(p + 1) != ' ') {
                 output[index] = *p;
-                index++;
             }
-            // 若左括號 ( 後方是空格，則 p 會多跳過一格
+            index++;
             p++;
         }
+        // 處理遇到其他標點符號的問題
         else if (*p == '.' || *p == ',' || *p == '!' || *p == ')')
         {
             // 若其餘標點符號前面有空格，則刪除空格
-            if (*(p - 1) == ' ') 
+            if (*(p - 1) == ' ') {
                 index--;
+            }
             output[index] = *p;
             index++;
             // 若其餘標點符號後方無空格，則視情況添加
             if (*(p + 1) != ' ') 
             {
                 // 要補空格的情況
-                if (*(p + 1) != '\0' && ((*(p + 1) != '.') && (*(p + 1) != ',') && (*(p + 1) != '!') && (*(p + 1) != ')') ))
+                if (*(p + 1) != '\0' && (*(p + 1) != '.' && *(p + 1) != ',' && *(p + 1) != '!'))
                 {
                     output[index] = ' ';
                     index++;
@@ -79,10 +81,6 @@ void stringProcessing(char* input, char* output)
             output[index] = *p;
             index++;
             p++;
-        }
-        if (*(p+1) == '\0')
-        {
-            output[index] = '\0';
         }
     }
 }
